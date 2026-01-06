0ae77ef7ca28423ecbc06e380cdc8a944c3e5bed
Dhruval Patel <pateldhruvalpatel00@gmail.com>
2026-01-06 22:26:44 +0530

    changes

--- scripts/claude/expriment6/flatetrade/test_login.py
+++ scripts/claude/expriment6/flatetrade/test_login.py
@@ -10,7 +10,7 @@ api = NorenApiPy()
 #set token and user id
 #paste the token generated using the login flow described 
 # in LOGIN FLOW of https://pi.flattrade.in/docs
-usersession='03514367a294fd228af0e955424a4f48d2a11a81e927a2fa58e5f7d97194d672'
+usersession='a318678a850c0467132b857b2f8ab38811708d950c3a68d54029d94b2bd875a9'
 userid = 'FZ31397'
 
 ret = api.set_session(userid= userid, password = '', usertoken= usersession)
